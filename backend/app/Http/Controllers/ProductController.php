a3090d76b731a8074e131530e159b3ec9868e0c3
dainji04 <ndai31072004@gmail.com>
2025-10-24 13:33:59 +0700

    backend: api get all products

--- backend/app/Http/Controllers/ProductController.php
+++ backend/app/Http/Controllers/ProductController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
-    //
+    public function getAllProducts() {
+        $products = DB::table('products')->simplePaginate(5);
+        return response()->json($products);
+    }
 }
